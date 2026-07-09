8251cb92dd260779e14388531e29c537846d27b9
silentstorm <mccurdyriley@protonmail.com>
2026-07-08 18:17:32 -0600

    Initial Commit

---
+++ dashboard/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+@login_required
+def main_view(requests):
+    return render(requests, "base.html")
